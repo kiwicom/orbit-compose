a078a2815ba4603eb693c0f82fcdcb4b0f2d890c
Jan Skrasek <jan.skrasek@kiwi.com>
2020-10-28 21:54:14 +0100

    code style in Stepper

--- app/src/main/java/kiwi/orbit/app/screens/Stepper.kt
+++ app/src/main/java/kiwi/orbit/app/screens/Stepper.kt
@@ -4,7 +4,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
