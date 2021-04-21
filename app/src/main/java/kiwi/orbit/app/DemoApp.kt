b08ee708a5dac005ea22ab035ee21c951fd121f3
Jan Skrasek <jan.skrasek@kiwi.com>
2021-04-21 21:06:34 +0200

    update compose to 1.0.0-beta05, accompanist and activity-compose

--- app/src/main/java/kiwi/orbit/app/DemoApp.kt
+++ app/src/main/java/kiwi/orbit/app/DemoApp.kt
@@ -1,7 +1,6 @@
 package kiwi.orbit.app
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,31 +8,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.systemuicontroller.LocalSystemUiController
-import com.google.accompanist.systemuicontroller.rememberAndroidSystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kiwi.orbit.app.ui.AppTheme
 
 @Composable
 fun DemoApp() {
     ProvideWindowInsets {
-        val systemUiController = rememberAndroidSystemUiController()
-        CompositionLocalProvider(LocalSystemUiController provides systemUiController) {
-            var isLightTheme by remember { mutableStateOf(true) }
+        var isLightTheme by remember { mutableStateOf(true) }
+        val systemUiController = rememberSystemUiController()
 
-            SideEffect {
-                systemUiController.setSystemBarsColor(
-                    color = Color.Transparent,
-                    darkIcons = isLightTheme,
-                )
-            }
+        SideEffect {
+            systemUiController.setSystemBarsColor(
+                color = Color.Transparent,
+                darkIcons = isLightTheme,
+            )
+        }
 
-            AppTheme(isLightTheme = isLightTheme) {
-                NavGraph(
-                    onToggleTheme = {
-                        isLightTheme = !isLightTheme
-                    }
-                )
-            }
+        AppTheme(isLightTheme = isLightTheme) {
+            NavGraph(
+                onToggleTheme = {
+                    isLightTheme = !isLightTheme
+                }
+            )
         }
     }
 }
