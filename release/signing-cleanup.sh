1d9e09cda8feeeb9c04aeaafce4bd04534278e64
Jan Skrasek <jan.skrasek@kiwi.com>
2021-09-10 14:04:40 +0200

    release: autoupload to GooglePlay

--- release/signing-cleanup.sh
+++ release/signing-cleanup.sh
@@ -1,3 +1,4 @@
 rm -f release/*.gpg
 rm -f release/*.properties
 rm -f release/*.jks
+rm -f release/*.json
--- release/signing-unpack.sh
+++ release/signing-unpack.sh
@@ -1,10 +1,12 @@
-ENCRYPT_KEY=$1
+read -sp "Enter the encrypt key: " ENCRYPT_KEY
+echo
 
 if [[ -n "$ENCRYPT_KEY" ]]; then
   openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in release/secring.gpg.aes -out release/secring.gpg -k ${ENCRYPT_KEY}
   openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in release/signing.properties.aes -out release/signing.properties -k ${ENCRYPT_KEY}
   openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in release/upload.jks.aes -out release/upload.jks -k ${ENCRYPT_KEY}
+  openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in release/service-account.json.aes -out release/service-account.json -k ${ENCRYPT_KEY}
 
 else
-  echo "password arg is empty"
+  echo "Encrypt key is empty"
 fi
