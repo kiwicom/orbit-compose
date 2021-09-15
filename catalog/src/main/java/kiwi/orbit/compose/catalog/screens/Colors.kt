84a21bba85592bc4d9f33871f8abc941936e32f5
Jan Skrasek <jan.skrasek@kiwi.com>
2021-09-15 08:41:22 +0200

    catalog: show all colors

--- catalog/src/main/java/kiwi/orbit/compose/catalog/screens/Colors.kt
+++ catalog/src/main/java/kiwi/orbit/compose/catalog/screens/Colors.kt
@@ -1,6 +1,7 @@
 package kiwi.orbit.compose.catalog.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,12 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.takeOrElse
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kiwi.orbit.compose.catalog.SubScreen
 import kiwi.orbit.compose.ui.OrbitTheme
 import kiwi.orbit.compose.ui.controls.Card
-import kiwi.orbit.compose.ui.controls.Surface
 import kiwi.orbit.compose.ui.controls.Text
 import kiwi.orbit.compose.ui.foundation.FeatureColors
 import kiwi.orbit.compose.ui.foundation.contentColorFor
@@ -26,52 +27,109 @@ fun ColorsScreen(onUpClick: () -> Unit) {
     SubScreen(
         title = "Colors",
         onUpClick = onUpClick,
+        withBackground = false,
     ) {
-        Surface {
-            Column(
-                Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-            ) {
-                ColorsScreenInner()
-            }
-        }
+        ColorsScreenInner()
     }
 }
 
 @Preview
 @Composable
 fun ColorsScreenInner() {
-    FeatureColors(OrbitTheme.colors.primary, name = "Primary")
-    FeatureColors(OrbitTheme.colors.interactive, name = "Interactive")
-    FeatureColors(OrbitTheme.colors.success, name = "Success")
-    FeatureColors(OrbitTheme.colors.warning, name = "Warning")
-    FeatureColors(OrbitTheme.colors.critical, name = "Critical")
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+    ) {
+        SurfaceColors()
+        ContentColors()
+        FeatureColors(OrbitTheme.colors.primary, name = "Primary")
+        FeatureColors(OrbitTheme.colors.interactive, name = "Interactive")
+        FeatureColors(OrbitTheme.colors.success, name = "Success")
+        FeatureColors(OrbitTheme.colors.warning, name = "Warning")
+        FeatureColors(OrbitTheme.colors.critical, name = "Critical")
+    }
 }
 
 @Composable
-private fun FeatureColors(colors: FeatureColors, name: String) {
-    Text(name, Modifier.padding(top = 16.dp, bottom = 4.dp), style = OrbitTheme.typography.title4)
+private fun SurfaceColors() {
+    Column {
+        Title("Surface")
 
-    Card {
-        Column {
-            Row {
-                Color(colors.subtle, "$name Subtle")
-                Color(colors.subtleAlt, "$name Subtle Alt")
-            }
-            Row {
-                Color(colors.main, "$name Main")
-                Color(colors.mainAlt, "$name Main Alt")
-            }
-            Row {
-                Color(colors.strong, "$name Strong")
+        val colors = OrbitTheme.colors.surface
+        Card {
+            Column {
+                Row {
+                    Color(colors.main, "Surface Main")
+                    Color(colors.background, "Surface Background")
+                }
+                Row {
+                    Color(colors.subtle, "Surface Subtle")
+                    Color(colors.strong, "Surface Strong")
+                }
+                Row {
+                    Color(colors.disabled, "Surface Disabled")
+                }
             }
         }
     }
 }
 
 @Composable
-private fun RowScope.Color(color: Color, name: String) {
+private fun ContentColors() {
+    Column {
+        Title("Content")
+
+        val colors = OrbitTheme.colors.content
+        val contentColor = OrbitTheme.colors.surface.main
+        Card {
+            Column {
+                Row {
+                    Color(colors.normal, "Content Normal", contentColor)
+                }
+                Row {
+                    Color(colors.minor, "Content Minor", contentColor)
+                    Color(colors.subtle, "Content Subtle", contentColor)
+                }
+                Row {
+                    Color(colors.disabled, "Content Disabled", contentColor)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun FeatureColors(colors: FeatureColors, name: String) {
+    Column {
+        Title(name)
+
+        Card {
+            Column {
+                Row {
+                    Color(colors.subtle, "$name Subtle")
+                    Color(colors.subtleAlt, "$name Subtle Alt")
+                }
+                Row {
+                    Color(colors.main, "$name Main")
+                    Color(colors.mainAlt, "$name Main Alt")
+                }
+                Row {
+                    Color(colors.strong, "$name Strong")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Title(name: String) {
+    Text(name, Modifier.padding(bottom = 4.dp), style = OrbitTheme.typography.title3)
+}
+
+@Composable
+private fun RowScope.Color(color: Color, name: String, contentColor: Color = Color.Unspecified) {
     Box(
         Modifier
             .background(color)
@@ -81,8 +139,7 @@ private fun RowScope.Color(color: Color, name: String) {
         Text(
             name,
             Modifier.padding(horizontal = 6.dp, vertical = 4.dp),
-            color = contentColorFor(color),
-            style = OrbitTheme.typography.bodySmall
+            color = contentColor.takeOrElse { contentColorFor(color) },
         )
     }
 }
