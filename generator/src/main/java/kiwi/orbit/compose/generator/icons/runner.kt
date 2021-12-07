1c950e31aca1506a10f67149c08bdeba8edcb943
Jan Skrasek <jan.skrasek@kiwi.com>
2021-12-07 22:09:02 +0100

    generator: use auto-updated zip with icons to update svgs

--- generator/src/main/java/kiwi/orbit/compose/generator/icons/runner.kt
+++ generator/src/main/java/kiwi/orbit/compose/generator/icons/runner.kt
@@ -8,7 +8,7 @@ fun main() {
     val root = Paths.get("").toAbsolutePath().toFile().absolutePath.removeSuffix("generator")
     val kotlinOutDir = Paths.get(root, "icons/src/main/java")
     val resourceOutDir = Paths.get("$root/icons/src/main/res/drawable/")
-    val urlIcons = "https://images.kiwi.com/orbit-icons/orbit-svgs.zip"
+    val urlIcons = "https://unpkg.com/@kiwicom/orbit-components%40latest/orbit-svgs.zip"
 
     processor.build(urlIcons, kotlinOutDir, resourceOutDir)
 }
