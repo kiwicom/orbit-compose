c8e8213463c549bb70dd1965961159c818f30fe2
Jan Skrasek <jan.skrasek@kiwi.com>
2022-01-13 14:18:30 +0100

    update tokens

--- .github/tokens-updater.sh
+++ .github/tokens-updater.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
-set -o pipefail
-set -u
+set -euo pipefail
 
 PR_COUNT=$(gh pr list --json id --label "design-tokens-autoupdate" --jq length -s open)
 
 if [[ $PR_COUNT -ne 0 ]]; then
   echo "Design Tokens Update pull-request is already created."
-  exit 0
+  exit 1
 fi
-
-TODAY=$(date '+%Y-%m-%d')
-BRANCH="tokens-autoupdate-$TODAY"
 
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
-git checkout -B $BRANCH
+git checkout -B tokens-autoupdate
 
 ./gradlew generator:colors
-git add --all
-git commit -m "tokens: update colors on $TODAY"
-COLORS_STATE=$?
+git add --all || true
+git commit -m "tokens: update colors on $TODAY" || true
 
 ./gradlew generator:icons
-git add --all
-git commit -m "tokens: update icons on $TODAY"
-ICONS_STATE=$?
-
-if [[ $COLORS_STATE == 0 ]] || [[ $ICONS_STATE == 0 ]]; then
-  git push -f origin "$BRANCH"
-  gh pr create --title "Design Tokens Update on $TODAY" --body "" --label "design-tokens-autoupdate" --label "feature" --base "main" --head "$BRANCH"
-fi
+git add --all || true
+git commit -m "tokens: update icons on $TODAY" || true
