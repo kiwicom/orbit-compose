9898044f16f1354f423d8b2bb3f56ad5813760bb
Jan Skrasek <jan.skrasek@kiwi.com>
2022-03-11 14:57:25 +0100

    buttons: drop elevation

--- ui/src/main/java/kiwi/orbit/compose/ui/controls/Button.kt
+++ ui/src/main/java/kiwi/orbit/compose/ui/controls/Button.kt
@@ -1,17 +1,7 @@
 package kiwi.orbit.compose.ui.controls
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.AnimationSpec
-import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.TweenSpec
-import androidx.compose.animation.core.VectorConverter
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.interaction.DragInteraction
-import androidx.compose.foundation.interaction.Interaction
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -20,14 +10,9 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.semantics.Role
@@ -40,7 +25,6 @@ import kiwi.orbit.compose.ui.foundation.LocalAlertScope
 import kiwi.orbit.compose.ui.foundation.ProvideContentEmphasis
 import kiwi.orbit.compose.ui.foundation.ProvideMergedTextStyle
 import kiwi.orbit.compose.ui.foundation.contentColorFor
-import kotlinx.coroutines.flow.collect
 
 @Composable
 public fun ButtonPrimary(
@@ -53,7 +37,6 @@ public fun ButtonPrimary(
         onClick = onClick,
         backgroundColor = OrbitTheme.colors.primary.main,
         modifier = modifier,
-        elevation = ButtonDefaults.elevation(),
         contentPadding = contentPadding,
         content = content,
     )
@@ -70,10 +53,6 @@ public fun ButtonPrimarySubtle(
         onClick = onClick,
         backgroundColor = OrbitTheme.colors.primary.subtle,
         modifier = modifier,
-        elevation = ButtonDefaults.elevation(
-            defaultElevation = 1.dp,
-            pressedElevation = 4.dp,
-        ),
         contentPadding = contentPadding,
         content = content,
     )
@@ -90,10 +69,6 @@ public fun ButtonSecondary(
         onClick = onClick,
         backgroundColor = OrbitTheme.colors.surface.strong,
         modifier = modifier,
-        elevation = ButtonDefaults.elevation(
-            defaultElevation = 1.dp,
-            pressedElevation = 2.dp,
-        ),
         contentPadding = contentPadding,
         content = content,
     )
@@ -110,7 +85,6 @@ public fun ButtonCritical(
         onClick = onClick,
         backgroundColor = OrbitTheme.colors.critical.main,
         modifier = modifier,
-        elevation = ButtonDefaults.elevation(),
         contentPadding = contentPadding,
         content = content,
     )
@@ -127,10 +101,6 @@ public fun ButtonCriticalSubtle(
         onClick = onClick,
         backgroundColor = OrbitTheme.colors.critical.subtle,
         modifier = modifier,
-        elevation = ButtonDefaults.elevation(
-            defaultElevation = 1.dp,
-            pressedElevation = 4.dp,
-        ),
         contentPadding = contentPadding,
         content = content,
     )
@@ -147,11 +117,6 @@ public fun ButtonLink(
         onClick = onClick,
         backgroundColor = Color.Transparent,
         contentColor = OrbitTheme.colors.primary.main,
-        elevation = ButtonDefaults.elevation(
-            defaultElevation = 0.dp,
-            pressedElevation = 0.dp,
-            disabledElevation = 0.dp,
-        ),
         modifier = modifier,
         contentPadding = contentPadding,
         content = content,
@@ -165,18 +130,12 @@ private fun Button(
     modifier: Modifier = Modifier,
     contentColor: Color = contentColorFor(backgroundColor),
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    elevation: ButtonElevation? = ButtonDefaults.elevation(),
     shape: Shape = OrbitTheme.shapes.normal,
     border: BorderStroke? = null,
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit
 ) {
     val inAlertScope = LocalAlertScope.current
-    val resolvedElevation = if (inAlertScope) {
-        ButtonDefaults.elevation(0.dp, 0.dp, 0.dp)
-    } else {
-        elevation
-    }
 
     Surface(
         modifier = modifier,
@@ -184,7 +143,6 @@ private fun Button(
         color = backgroundColor,
         contentColor = contentColor,
         border = border,
-        elevation = resolvedElevation?.elevation(enabled = true, interactionSource)?.value ?: 0.dp,
         onClick = onClick,
         role = Role.Button,
         interactionSource = interactionSource,
@@ -217,155 +175,6 @@ private fun Button(
     }
 }
 
-@Stable
-public interface ButtonElevation {
-    /**
-     * Represents the elevation used in a button, depending on [enabled] and
-     * [interactionSource].
-     *
-     * @param enabled whether the button is enabled
-     * @param interactionSource the [InteractionSource] for this button
-     */
-    @Composable
-    public fun elevation(enabled: Boolean, interactionSource: InteractionSource): State<Dp>
-}
-
-@Stable
-private class DefaultButtonElevation(
-    private val defaultElevation: Dp,
-    private val pressedElevation: Dp,
-    private val disabledElevation: Dp,
-) : ButtonElevation {
-    @Composable
-    override fun elevation(enabled: Boolean, interactionSource: InteractionSource): State<Dp> {
-        val interactions = remember { mutableStateListOf<Interaction>() }
-        LaunchedEffect(interactionSource) {
-            interactionSource.interactions.collect { interaction ->
-                when (interaction) {
-                    is PressInteraction.Press -> {
-                        interactions.add(interaction)
-                    }
-                    is PressInteraction.Release -> {
-                        interactions.remove(interaction.press)
-                    }
-                    is PressInteraction.Cancel -> {
-                        interactions.remove(interaction.press)
-                    }
-                }
-            }
-        }
-
-        val interaction = interactions.lastOrNull()
-
-        val target = if (!enabled) {
-            disabledElevation
-        } else {
-            when (interaction) {
-                is PressInteraction.Press -> pressedElevation
-                else -> defaultElevation
-            }
-        }
-
-        val animatable = remember { Animatable(target, Dp.VectorConverter) }
-
-        if (!enabled) {
-            // No transition when moving to a disabled state
-            LaunchedEffect(target) {
-                animatable.snapTo(target)
-            }
-        } else {
-            LaunchedEffect(target) {
-                val lastInteraction = when (animatable.targetValue) {
-                    pressedElevation -> PressInteraction.Press(Offset.Zero)
-                    else -> null
-                }
-                animatable.animateElevation(
-                    from = lastInteraction,
-                    to = interaction,
-                    target = target
-                )
-            }
-        }
-
-        return animatable.asState()
-    }
-}
-
-internal suspend fun Animatable<Dp, *>.animateElevation(
-    target: Dp,
-    from: Interaction? = null,
-    to: Interaction? = null
-) {
-    val spec = when {
-        // Moving to a new state
-        to != null -> ElevationDefaults.incomingAnimationSpecForInteraction(to)
-        // Moving to default, from a previous state
-        from != null -> ElevationDefaults.outgoingAnimationSpecForInteraction(from)
-        // Loading the initial state, or moving back to the baseline state from a disabled /
-        // unknown state, so just snap to the final value.
-        else -> null
-    }
-    if (spec != null) animateTo(target, spec) else snapTo(target)
-}
-
-/**
- * Contains default [AnimationSpec]s used for animating elevation between different [Interaction]s.
- *
- * Typically you should use [animateElevation] instead, which uses these [AnimationSpec]s
- * internally. [animateElevation] in turn is used by the defaults for [Button] and
- * [FloatingActionButton] - inside [ButtonDefaults.elevation] and
- * [FloatingActionButtonDefaults.elevation] respectively.
- *
- * @see animateElevation
- */
-private object ElevationDefaults {
-    /**
-     * Returns the [AnimationSpec]s used when animating elevation to [interaction], either from a
-     * previous [Interaction], or from the default state. If [interaction] is unknown, then
-     * returns `null`.
-     *
-     * @param interaction the [Interaction] that is being animated to
-     */
-    fun incomingAnimationSpecForInteraction(interaction: Interaction): AnimationSpec<Dp>? {
-        return when (interaction) {
-            is PressInteraction.Press -> DefaultIncomingSpec
-            is DragInteraction.Start -> DefaultIncomingSpec
-            else -> null
-        }
-    }
-
-    /**
-     * Returns the [AnimationSpec]s used when animating elevation away from [interaction], to the
-     * default state. If [interaction] is unknown, then returns `null`.
-     *
-     * @param interaction the [Interaction] that is being animated away from
-     */
-    fun outgoingAnimationSpecForInteraction(interaction: Interaction): AnimationSpec<Dp>? {
-        return when (interaction) {
-            is PressInteraction.Press -> DefaultOutgoingSpec
-            is DragInteraction.Start -> DefaultOutgoingSpec
-            // TODO: use [HoveredOutgoingSpec] when hovered
-            else -> null
-        }
-    }
-}
-
-private val DefaultIncomingSpec = TweenSpec<Dp>(
-    durationMillis = 120,
-    easing = FastOutSlowInEasing
-)
-
-private val DefaultOutgoingSpec = TweenSpec<Dp>(
-    durationMillis = 150,
-    easing = CubicBezierEasing(0.40f, 0.00f, 0.60f, 1.00f)
-)
-
-@Suppress("unused")
-private val HoveredOutgoingSpec = TweenSpec<Dp>(
-    durationMillis = 120,
-    easing = CubicBezierEasing(0.40f, 0.00f, 0.60f, 1.00f)
-)
-
 public object ButtonDefaults {
     private val ButtonHorizontalPadding = 16.dp
     private val ButtonVerticalPadding = 8.dp
@@ -393,33 +202,4 @@ public object ButtonDefaults {
     public val MinHeight: Dp = 44.dp
 
     public val MinAlertHeight: Dp = 32.dp
-
-    // TODO: b/152525426 add support for focused and hovered states
-
-    /**
-     * Creates a [ButtonElevation] that will animate between the provided values according to the
-     * Material specification for a [Button].
-     *
-     * @param defaultElevation the elevation to use when the [Button] is enabled, and has no
-     * other [Interaction]s.
-     * @param pressedElevation the elevation to use when the [Button] is enabled and
-     * is pressed.
-     * @param disabledElevation the elevation to use when the [Button] is not enabled.
-     */
-    @Composable
-    public fun elevation(
-        defaultElevation: Dp = 2.dp,
-        pressedElevation: Dp = 8.dp,
-        // focused: Dp = 4.dp,
-        // hovered: Dp = 4.dp,
-        disabledElevation: Dp = 0.dp
-    ): ButtonElevation {
-        return remember(defaultElevation, pressedElevation, disabledElevation) {
-            DefaultButtonElevation(
-                defaultElevation = defaultElevation,
-                pressedElevation = pressedElevation,
-                disabledElevation = disabledElevation
-            )
-        }
-    }
 }
