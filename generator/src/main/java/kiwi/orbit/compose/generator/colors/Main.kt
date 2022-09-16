12ee256ee87fd9b926fdab20500e1925ee7bd7b3
Jan Skrasek <jan.skrasek@kiwi.com>
2022-09-16 20:10:02 +0200

    generator: download colors directly from Figma file

--- generator/src/main/java/kiwi/orbit/compose/generator/colors/Main.kt
+++ generator/src/main/java/kiwi/orbit/compose/generator/colors/Main.kt
@@ -5,9 +5,9 @@ import java.nio.file.Paths
 fun main() {
     val processor = ColorsGenerator()
 
+    val figmaToken = System.getenv("FIGMA_TOKEN")
     val root = Paths.get("").toAbsolutePath().toFile().absolutePath.removeSuffix("generator")
     val kotlinOutDir = Paths.get(root, "ui/src/main/java/")
-    val urlColors = "https://unpkg.com/%40kiwicom/orbit-design-tokens%40latest/output/theo-spec.json"
 
-    processor.build(urlColors, kotlinOutDir)
+    processor.build(figmaToken, kotlinOutDir)
 }
