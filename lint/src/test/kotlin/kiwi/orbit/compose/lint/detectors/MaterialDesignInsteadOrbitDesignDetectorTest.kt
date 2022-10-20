b27667ef096e1b0cc0992e4832c0f309c3eb778f
Jan Skrasek <jan.skrasek@kiwi.com>
2022-10-20 13:27:51 +0200

    add more details unit-test for lint

--- lint/src/test/kotlin/kiwi/orbit/compose/lint/detectors/MaterialDesignInsteadOrbitDesignDetectorTest.kt
+++ lint/src/test/kotlin/kiwi/orbit/compose/lint/detectors/MaterialDesignInsteadOrbitDesignDetectorTest.kt
@@ -1,8 +1,17 @@
 package kiwi.orbit.compose.lint.detectors
 
+import com.android.tools.lint.LintCliClient
+import com.android.tools.lint.LintStats
+import com.android.tools.lint.Reporter
 import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
 import com.android.tools.lint.checks.infrastructure.TestMode
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.Incident
+import com.google.common.io.Files
+import java.io.BufferedWriter
+import java.io.File
+import org.junit.Assert
 import org.junit.Test
 
 @Suppress("UnstableApiUsage")
@@ -36,12 +45,44 @@ class MaterialDesignInsteadOrbitDesignDetectorTest {
             """.trimIndent(),
         )
 
-        lint()
+        val result = lint()
             .detector(MaterialDesignInsteadOrbitDesignDetector())
             .issues(MaterialDesignInsteadOrbitDesignDetector.ISSUE)
             .testModes(TestMode.FULLY_QUALIFIED)
+            .allowMissingSdk()
             .files(stubFile, materialFile, materialIconsFile)
             .run()
-            .expectErrorCount(3)
+
+        val messages = mutableListOf<String>()
+        result.checkReport(
+            { lintClient, file -> MessageReporter(lintClient, file, messages) },
+            ".custom",
+        )
+
+        Assert.assertSame(3, messages.count())
+        Assert.assertEquals(
+            listOf(
+                "Using androidx.compose.material.Card instead of kiwi.orbit.compose.ui.controls.SurfaceCard",
+                "Using androidx.compose.material.Text instead of kiwi.orbit.compose.ui.controls.Text",
+                "Using androidx.compose.material.icons.Icons instead of kiwi.orbit.compose.icons.Icons",
+            ),
+            messages,
+        )
+    }
+
+    class MessageReporter(
+        client: LintCliClient,
+        output: File,
+        private val messages: MutableList<String>,
+    ) : Reporter(client, output) {
+        init {
+            BufferedWriter(Files.newWriter(output, Charsets.UTF_8)).use {
+                it.newLine()
+            }
+        }
+
+        override fun write(stats: LintStats, incidents: List<Incident>, registry: IssueRegistry) {
+            messages.addAll(incidents.map { it.message })
+        }
     }
 }
