6ded3117c3bdec610f63f5c1e1081cc17b2e5835
Jan Skrasek <jan.skrasek@kiwi.com>
2022-10-26 09:45:21 +0200

    catalog: fix Ciruclar20 setup applied if font is available

--- catalog/src/main/java/kiwi/orbit/compose/catalog/AppTheme.kt
+++ catalog/src/main/java/kiwi/orbit/compose/catalog/AppTheme.kt
@@ -5,11 +5,9 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import kiwi.orbit.compose.ui.OrbitTheme
 import kiwi.orbit.compose.ui.foundation.Typography
 import kiwi.orbit.compose.ui.foundation.createTypography
@@ -31,16 +29,15 @@ fun AppTheme(
     )
 }
 
-@OptIn(ExperimentalTextApi::class)
 @SuppressLint("DiscouragedApi")
 @Composable
 private fun rememberTypography(context: Context): Typography = remember {
     val resources = context.resources
     val packageName = context.packageName
 
-    val book = resources.getIdentifier("circular_pro_book", "font", packageName)
-    val medium = resources.getIdentifier("circular_pro_medium", "font", packageName)
-    val bold = resources.getIdentifier("circular_pro_bold", "font", packageName)
+    val book = resources.getIdentifier("circular20_book", "font", packageName)
+    val medium = resources.getIdentifier("circular20_medium", "font", packageName)
+    val bold = resources.getIdentifier("circular20_bold", "font", packageName)
 
     if (book == 0 || medium == 0 || bold == 0) {
         return@remember createTypography()
@@ -51,11 +48,6 @@ private fun rememberTypography(context: Context): Typography = remember {
             Font(book, FontWeight.Normal),
             Font(medium, FontWeight.Medium),
             Font(bold, FontWeight.Bold),
-        ),
-        defaultLineHeightStyle = LineHeightStyle(
-            // Accommodate for Circular Pro to 0.6f instead of Center
-            LineHeightStyle.Alignment(0.6f),
-            LineHeightStyle.Trim.None,
         ),
     )
 }
