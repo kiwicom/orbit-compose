c1d91db0613f47c2c9763e3aa395dd961fc412f0
Jan Skrasek <jan.skrasek@kiwi.com>
2023-07-01 13:07:26 +0200

    multiplatform: enable in :icons

--- generator/src/main/java/kiwi/orbit/compose/generator/icons/Main.kt
+++ generator/src/main/java/kiwi/orbit/compose/generator/icons/Main.kt
@@ -1,13 +1,14 @@
 package kiwi.orbit.compose.generator.icons
 
 import java.nio.file.Paths
+import kotlin.io.path.div
 
 fun main() {
     val processor = IconsGenerator()
 
-    val root = Paths.get("").toAbsolutePath().toFile().absolutePath.removeSuffix("generator")
-    val kotlinOutDir = Paths.get(root, "icons/src/main/java")
-    val resourceOutDir = Paths.get("$root/icons/src/main/res/drawable/")
+    val root = Paths.get(Paths.get("").toAbsolutePath().toFile().absolutePath.removeSuffix("generator"))
+    val kotlinOutDir = root / "icons/src/androidMain/kotlin/"
+    val resourceOutDir = root / "icons/src/androidMain/res/drawable/"
     val urlIcons = "https://unpkg.com/@kiwicom/orbit-components%40latest/orbit-svgs.zip"
 
     processor.build(urlIcons, kotlinOutDir, resourceOutDir)
