90ab2dff54318a3446fc402271ae1ed3900f7366
Bořek Leikep <borek.leikep@kiwi.com>
2023-11-15 11:19:13 +0100

    Catalog Icons and Illustrations screens no longer use reflection to list all items.

--- catalog/src/main/java/kiwi/orbit/compose/catalog/screens/IllustrationsScreen.kt
+++ catalog/src/main/java/kiwi/orbit/compose/catalog/screens/IllustrationsScreen.kt
@@ -9,21 +9,19 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentComposer
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kiwi.orbit.compose.catalog.semantics.SubScreenSemantics
-import kiwi.orbit.compose.illustrations.Illustrations
+import kiwi.orbit.compose.illustrations.IllustrationName
+import kiwi.orbit.compose.illustrations.painter
 import kiwi.orbit.compose.ui.OrbitTheme
 import kiwi.orbit.compose.ui.controls.Scaffold
 import kiwi.orbit.compose.ui.controls.SurfaceCard
 import kiwi.orbit.compose.ui.controls.Text
 import kiwi.orbit.compose.ui.controls.TopAppBar
 import kiwi.orbit.compose.ui.utils.plus
-import kotlin.reflect.full.memberProperties
 
 @Composable
 internal fun IllustrationsScreen(onNavigateUp: () -> Unit) {
@@ -42,29 +40,25 @@ internal fun IllustrationsScreen(onNavigateUp: () -> Unit) {
 
 @Composable
 private fun IllustrationsScreenInner(contentPadding: PaddingValues) {
-    val illustrations: List<Pair<String, Painter>> = Illustrations::class.memberProperties.map {
-        it.name to (it.getter.call(Illustrations, currentComposer, 0) as Painter)
-    }
-
     LazyVerticalGrid(
         columns = GridCells.Adaptive(256.dp),
         contentPadding = contentPadding + PaddingValues(8.dp),
     ) {
-        items(illustrations) { (name, icon) ->
+        items(IllustrationName.values()) { illustration ->
             SurfaceCard(Modifier.padding(8.dp)) {
                 Column {
                     Text(
-                        name,
+                        illustration.name,
                         Modifier.padding(top = 4.dp, start = 6.dp),
                         style = OrbitTheme.typography.bodyNormal,
                         textAlign = TextAlign.Center,
                     )
                     Image(
-                        painter = icon,
+                        painter = illustration.painter(),
                         modifier = Modifier
                             .padding(8.dp)
                             .fillMaxWidth(),
-                        contentDescription = name,
+                        contentDescription = illustration.name,
                     )
                 }
             }
