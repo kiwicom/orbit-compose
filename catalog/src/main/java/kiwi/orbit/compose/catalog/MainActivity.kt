47eb5c30b64f6a20415e75cd9c4c8c1e24a91f9c
Jan Skrasek <jan.skrasek@kiwi.com>
2024-02-29 09:49:58 +0100

    rewrite SystemUiController to edgeToEdge

--- catalog/src/main/java/kiwi/orbit/compose/catalog/MainActivity.kt
+++ catalog/src/main/java/kiwi/orbit/compose/catalog/MainActivity.kt
@@ -1,18 +1,43 @@
 package kiwi.orbit.compose.catalog
 
+import android.app.UiModeManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.getSystemService
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
+        enableEdgeToEdge()
         setContent {
-            CatalogApplication()
+            CatalogApplication(this)
         }
     }
+
+    internal fun setUiMode(isLight: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val uiModeManager = getSystemService<UiModeManager>()!!
+            uiModeManager.setApplicationNightMode(
+                if (isLight) UiModeManager.MODE_NIGHT_NO else UiModeManager.MODE_NIGHT_YES,
+            )
+        } else {
+            AppCompatDelegate.setDefaultNightMode(
+                if (isLight) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES,
+            )
+        }
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT) { !isLight },
+            navigationBarStyle = SystemBarStyle.auto(DefaultLightScrim, DefaultDarkScrim) { !isLight },
+        )
+    }
+
+    private val DefaultLightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+    private val DefaultDarkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b)
 }
